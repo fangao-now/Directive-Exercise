db97cf9b57ba52c45a61b24f84914c5377a6f075
coolcrazydog <fan.gao1989@gmail.com>
2019-03-03 13:26:06 -0500

    mouse over/mouse leave event implementation using HostListener

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,20 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.render.setStyle(this.elementRef.nativeElement, 'background-color', 'palegreen');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.render.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
 
   constructor(private elementRef: ElementRef, private render: Renderer2) { }
 
   ngOnInit() {
-    this.render.setStyle(this.elementRef.nativeElement, 'background-color', 'palegreen');
+    // this.render.setStyle(this.elementRef.nativeElement, 'background-color', 'palegreen');
   }
 }
